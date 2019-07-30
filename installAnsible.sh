872a7ffc7566370f6e9b69d705bd8dd874b9bdcc
Nancy Chen <38501271+rukawata@users.noreply.github.com>
2019-07-30 14:25:43 -0700

    Update installAnsible.sh

--- installAnsible.sh
+++ installAnsible.sh
@@ -3,3 +3,4 @@ apt update
 apt install software-properties-common
 apt-add-repository --yes --update ppa:ansible/ansible
 apt install -y ansible > /home/nancyc/install.log
+yes | git clone git@github.com:Azure/sap-hana.git /home/nancyc/git
