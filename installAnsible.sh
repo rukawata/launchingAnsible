b03bbfdad73d5c86c9dc31e91379601732db582a
Nancy Chen <38501271+rukawata@users.noreply.github.com>
2019-07-30 22:00:08 -0700

    Update installAnsible.sh

--- installAnsible.sh
+++ installAnsible.sh
@@ -4,3 +4,4 @@ apt-add-repository --yes --update ppa:ansible/ansible
 apt install -y ansible > /home/nancyc/install.log
 mkdir /home/nancyc/git
 yes | git clone git@github.com:Azure/sap-hana.git /home/nancyc/git
+ansible-playbook /home/nancyc/git/testAnsible/test_playbook.yml --extra-vars "url_hdbserver=https://hana238791421.blob.core.windows.net/sapbits/SAPCAR_1211-80000935.EXE"
